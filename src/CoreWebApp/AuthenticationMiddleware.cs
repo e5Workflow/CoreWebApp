baf4cc914b57491b496a02ec371a6fdd01a3a757
esaunders <esaunders@ESDEV1>
2016-11-25 14:51:26 +1100

    Add project files.

---
+++ src/CoreWebApp/AuthenticationMiddleware.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.Extensions.Logging;
+
+namespace CoreWebApp
+{
+    public class AuthenticationOptions
+    {
+        private string _authenticationScheme;
+
+        /// <summary>
+        /// The AuthenticationScheme in the options corresponds to the logical name for a particular authentication scheme. A different
+        /// value may be assigned in order to use the same authentication middleware type more than once in a pipeline.
+        /// </summary>
+        public string AuthenticationScheme
+        {
+            get { return _authenticationScheme; }
+            set
+            {
+                _authenticationScheme = value;
+                Description.AuthenticationScheme = value;
+            }
+        }
+
+        /// <summary>
+        /// If true the authentication middleware alter the request user coming in. If false the authentication middleware will only provide
+        /// identity when explicitly indicated by the AuthenticationScheme.
+        /// </summary>
+        public bool AutomaticAuthenticate { get; set; }
+
+        /// <summary>
+        /// If true the authentication middleware should handle automatic challenge.
+        /// If false the authentication middleware will only alter responses when explicitly indicated by the AuthenticationScheme.
+        /// </summary>
+        public bool AutomaticChallenge { get; set; }
+
+        /// <summary>
+        /// Gets or sets the issuer that should be used for any claims that are created
+        /// </summary>
+        public string ClaimsIssuer { get; set; }
+
+        /// <summary>
+        /// Additional information about the authentication type which is made available to the application.
+        /// </summary>
+        public AuthenticationDescription Description { get; set; } = new AuthenticationDescription();
+        
+    }
+
+
+    public class AuthenticationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly AuthenticationOptions _options;
+
+        public AuthenticationMiddleware(
+            RequestDelegate next,
+            IOptions<AuthenticationOptions> options)
+        {
+            _next = next;
+            _options = options.Value;
+        }
+
+        public ILogger Logger { get; set; }
+
+        public UrlEncoder UrlEncoder { get; set; }
+
+        public async Task Invoke(HttpContext context)
+        {
+            this.BeginInvoke(context);
+            await this._next.Invoke(context);
+        }
+
+
+        private void BeginInvoke(HttpContext context)
+        {
+
+            //The OPTIONS request does not reach this far...
+
+            //This is to mitigate bug in Chrome - http://stackoverflow.com/questions/15734031/why-does-the-preflight-options-request-of-an-authenticated-cors-request-work-in
+            //if (!context.Response.Headers.ContainsKey("Access-Control-Allow-Credentials"))
+            //{
+            //    context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+            //}
+            //if (context.Request.Method != "OPTIONS" && !context.User.Identity.IsAuthenticated)
+            //{
+            //    context.Response.StatusCode = 401;
+            //}
+
+        }
+    }
+
+
+
+    public static class AuthenticationMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomAuthentication(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AuthenticationMiddleware>();
+        }
+    }
+}
---
+++ src/CoreWebApp/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features.Authentication;
+
+namespace CoreWebApp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add CORS service and create Policy with options - must be called before AddMvc()
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            // Add framework services.
+            services.AddMvc();
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseCustomAuthentication();
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            
+            // global policy - assign here or on each controller with [EnableCors("CorsPolicy")] attribute - call before UseMvc()
+            app.UseCors("CorsPolicy");
+
+            app.UseMvc();
+        }
+        
+    }
+}
