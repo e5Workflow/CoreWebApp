b8129981e64010729aee8f6e1e9a92447d378f45
esaunders <esaunders@e5workflow.com>
2016-11-25 15:46:39 +1100

    Fix CORS issue:
    Allow both WindowsAuthentication and AnonymousAuthentication
    In the 1st middleware check for authentication for all methods except OPTIONS (Preflight) and return a 401 where appropriate.

--- src/CoreWebApp/AuthenticationMiddleware.cs
+++ src/CoreWebApp/AuthenticationMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Principal;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -14,66 +15,11 @@ using Microsoft.Extensions.Logging;
 
 namespace CoreWebApp
 {
-    public class AuthenticationOptions
-    {
-        private string _authenticationScheme;
-
-        /// <summary>
-        /// The AuthenticationScheme in the options corresponds to the logical name for a particular authentication scheme. A different
-        /// value may be assigned in order to use the same authentication middleware type more than once in a pipeline.
-        /// </summary>
-        public string AuthenticationScheme
-        {
-            get { return _authenticationScheme; }
-            set
-            {
-                _authenticationScheme = value;
-                Description.AuthenticationScheme = value;
-            }
-        }
-
-        /// <summary>
-        /// If true the authentication middleware alter the request user coming in. If false the authentication middleware will only provide
-        /// identity when explicitly indicated by the AuthenticationScheme.
-        /// </summary>
-        public bool AutomaticAuthenticate { get; set; }
-
-        /// <summary>
-        /// If true the authentication middleware should handle automatic challenge.
-        /// If false the authentication middleware will only alter responses when explicitly indicated by the AuthenticationScheme.
-        /// </summary>
-        public bool AutomaticChallenge { get; set; }
-
-        /// <summary>
-        /// Gets or sets the issuer that should be used for any claims that are created
-        /// </summary>
-        public string ClaimsIssuer { get; set; }
-
-        /// <summary>
-        /// Additional information about the authentication type which is made available to the application.
-        /// </summary>
-        public AuthenticationDescription Description { get; set; } = new AuthenticationDescription();
-        
-    }
-
-
+    
     public class AuthenticationMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly AuthenticationOptions _options;
-
-        public AuthenticationMiddleware(
-            RequestDelegate next,
-            IOptions<AuthenticationOptions> options)
-        {
-            _next = next;
-            _options = options.Value;
-        }
-
-        public ILogger Logger { get; set; }
-
-        public UrlEncoder UrlEncoder { get; set; }
-
+        
         public async Task Invoke(HttpContext context)
         {
             this.BeginInvoke(context);
@@ -81,21 +27,15 @@ namespace CoreWebApp
         }
 
 
-        private void BeginInvoke(HttpContext context)
+        private async void BeginInvoke(HttpContext context)
         {
-
-            //The OPTIONS request does not reach this far...
-
             //This is to mitigate bug in Chrome - http://stackoverflow.com/questions/15734031/why-does-the-preflight-options-request-of-an-authenticated-cors-request-work-in
-            //if (!context.Response.Headers.ContainsKey("Access-Control-Allow-Credentials"))
-            //{
-            //    context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
-            //}
-            //if (context.Request.Method != "OPTIONS" && !context.User.Identity.IsAuthenticated)
-            //{
-            //    context.Response.StatusCode = 401;
-            //}
-
+            //See also https://github.com/aspnet/CORS/issues/60
+            if (context.Request.Method!="OPTIONS" && !context.User.Identity.IsAuthenticated)
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Not Authenticated");
+            }
         }
     }
 
